7d9bcef27c5ac0950093d58a9ecdb59b1a3e0dbc
SOLOMON AMPAH <109488683+Pizzosta@users.noreply.github.com>
2023-10-27 00:16:55 +0000

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
-'''Function to check unlocked boxes'''
+'''LockBoxes Challenge'''
 
 
 def canUnlockAll(boxes):
-    '''unlock boxes'''
-    visited = set()
-    visited.add(0)
-    queue = [0]
+    '''determines if all the boxes can be opened or not
+    Returns:
+        True: all boxes can be opened
+        False: not all boxes can be opened
+    '''
+    length = len(boxes)
+    keys = set()
+    opened_boxes = []
+    i = 0
 
-    while queue:
-        current_box = queue.pop(0)
+    while i < length:
+        oldi = i
+        opened_boxes.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < length and key not in opened_boxes:
+                i = key
+                break
+        if oldi != i:
+            continue
+        else:
+            break
 
-        for key in boxes[current_box]:
-            if key not in visited:
-                visited.add(key)
-                queue.append(key)
-
-    return len(visited) == len(boxes)
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
